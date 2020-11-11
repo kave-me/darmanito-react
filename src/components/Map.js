b7e0b34106f7c4eb1817e3e53bdcf88a445a54f6
Kave Mohammadi <kave.mdi@gmail.com>
2020-11-11 11:59:07 +0330

    add map to contactUs page

---
+++ src/components/Map.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import NeshanMap from 'react-neshan-map-leaflet';
+
+
+function SimpleMap() {
+  return (
+    <NeshanMap
+    options={{
+        key: 'web.PPrZCZOfUsRDnQvy8H6YJrWoAaQV7UOI6yDRY70e',
+        maptype: 'dreamy',
+        poi: true,
+        traffic: false,
+        center: [35.699739, 51.338097],
+        zoom: 13
+    }}
+    style= {{
+      width: '600px',
+      height: '480px',
+      margin: 0,
+      padding: 0,
+      borderRadius: '10px',
+      background: '#eee'
+    }}
+
+    onInit={(L, myMap) => {
+        let marker = L.marker([35.699739, 51.338097])
+        .addTo(myMap)
+        .bindPopup('I am a popup.');
+
+        myMap.on('click', function (e) {
+        marker.setLatLng(e.latlng)
+        });
+
+        L.circle([35.699739, 51.348097], {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        stroke: 'false',
+        radius: 50
+        }).addTo(myMap);
+
+        L.circle([35.699739, 51.348097], {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        stroke: 'false',
+        radius: 500
+        }).addTo(myMap);
+    }}
+    />
+  );
+}
+
+export default SimpleMap;
+
+
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,12 +2,30 @@ import React from 'react';
 import TextBanner from '../components/TextBanner'
 import Support from '../components/Support'
 import Footer from '../components/Footer'
+import SimpleMap from '../components/Map'
 
 const ContactUs = () => {
     return (
         <div className="container">
             <TextBanner text="تماس با ما"/>
-            <h1 className="TemprorayPlaceHolder">place holder</h1>
+            <div className="contactUsFormAndMap section">
+                <div className="messageForm">
+                    <form className="messageForm__form">
+                        <label className="messageForm__form__label" htmlFor="fullname">نام و نام خانوادگی</label>
+                        <input className="messageForm__form__input" id="fullname" name="fullname" type="text" placeholder="نام و نام خانوادگی"/>
+                        <label className="messageForm__form__label" htmlFor="emailAndPhone">ایمیل یا شماره همراه</label>
+                        <input className="messageForm__form__input" id="emailAndPhone" type="text" placeholder="ایمیل یا شماره همراه"/>
+
+                        <label className="messageForm__form__label" htmlFor="message">پیام شما</label>
+                        <textarea className="messageForm__form__textarea" name="name is here" id="message" cols="30" rows="9" placeholder="متن پیام"></textarea>
+
+                        <button className="messageForm__form__button">ارسال پیام</button>
+                    </form>
+                </div>
+                <div className="contactUS__map"> 
+                <SimpleMap />
+                </div>
+            </div>
             <Support/>
             <Footer/>
         </div>
