556125d435ade74221b7f525a0cd0abf2b5b8dea
Kave Mohammadi <kave.mdi@gmail.com>
2020-11-15 12:08:47 +0330

    add support and footer to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import MainHeader from '../components/MainHeader';
+import Support from '../components/Support';
+import Footer from '../components/Footer';
 
 const Main = () => {
     return (
         <div className="mainContainer container">
             <MainHeader/>
             <h1>Main page</h1>
-            
+            <Support/>
+            <Footer/>
         </div>
     );
 }
