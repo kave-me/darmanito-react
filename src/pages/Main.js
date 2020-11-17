1ac2e79ad478fd5f44ca15d0f705fa93575154c5
Kave Mohammadi <kave.mdi@gmail.com>
2020-11-17 03:32:52 +0330

    add mainFeatures section to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainHeader from '../components/MainHeader';
 import MainAbout from '../components/MainAbout';
+import MainFeatures from '../components/MainFeatures';
 import Support from '../components/Support';
 import Footer from '../components/Footer';
 
@@ -9,6 +10,7 @@ const Main = () => {
         <div className="mainContainer container">
             <MainHeader/>
             <MainAbout/>
+            <MainFeatures/>
             <h1>Main page</h1>
             <Support/>
             <Footer/>
