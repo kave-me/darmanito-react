018e3dfdff0eaa4867c2b7868b2e117ecbdc1006
Kave Mohammadi <kave.mdi@gmail.com>
2020-11-29 11:33:03 +0330

    make MainStatics responsive

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -10,7 +10,7 @@ const AboutUs = () => {
     return (
         <div className="container">
             <Header/>
-            {/* <Statics/> */}
+            <Statics/>
             {/* <Services/> */}
             <Support/>
             <Footer/>
