a10359f2e1a248dbde10f146e6aa4084bfdd5fc9
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-03 16:05:16 +0330

    add MainRegisterModal

--- src/components/MainRegister.js
+++ src/components/MainRegister.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import mainRegisterPic from '../asset/image/mainRegister-person.png';
+import RegisterModal from './RegisterModal';
 
 const MainRegister = () => {
+    const [viewModal, setViewModal] = useState(false);
     return(
         <div className="mainRegisterContainer">
             <div className="mainRegister">
@@ -11,10 +13,13 @@ const MainRegister = () => {
                     <p className="mainRegister__info__description">اگر به عنوان داروخانه یا مراکز درمانی قصد دارید در روند درمان و کسب و کار خودتان تغییر ایجاد کنید درمانیتو منتظر شماست.</p>
                     <div className="mainRegister__info__input">
                         <input type="text" className="mainRegister__info__input__input" placeholder="شماره همراه را وارد کنید 09127654321"/>
-                        <button className="mainRegister__info__input__button">ثبت نام سریع</button>
+                        <button onClick={() => setViewModal(true)} className="mainRegister__info__input__button">ثبت نام سریع</button>
                     </div>
                 </div>
             </div>
+            {viewModal
+             ? <RegisterModal click={() => setViewModal(!viewModal)}/>
+             : null}
 
         </div>
     );
