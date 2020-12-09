8241a42b6551d52b1ed6688151fd72bb98e65854
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-09 17:58:39 +0330

    add validation to mainRegister component

--- src/components/main/register/main-register.js
+++ src/components/main/register/main-register.js
@@ -2,8 +2,17 @@ import React, {useState} from 'react';
 import mainRegisterPic from '../../../asset/image/mainRegister-person.png';
 import RegisterModal from '../../base/modal/register-modal';
 import './main-register.scss';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const MainRegister = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: ""
+        },
+        validationSchema: Yup.object({
+            phoneNumber: Yup.number()
+            .typeError("شماره وارد شده صحیح نیست")})});
     const [viewModal, setViewModal] = useState(false);
     return(
         <div className="mainRegisterContainer">
@@ -13,9 +22,15 @@ const MainRegister = () => {
                     <h2 className="mainRegister__info__title">عضویت داروخانه یا مراکز درمانی</h2>
                     <p className="mainRegister__info__description">اگر به عنوان داروخانه یا مراکز درمانی قصد دارید در روند درمان و کسب و کار خودتان تغییر ایجاد کنید درمانیتو منتظر شماست.</p>
                     <div className="mainRegister__info__input">
-                        <input type="text" className="mainRegister__info__input__input" placeholder="شماره همراه را وارد کنید 09127654321"/>
-                        <button onClick={() => setViewModal(true)} className="mainRegister__info__input__button">ثبت نام سریع</button>
+                        <input type="text" className="mainRegister__info__input__input"
+                         placeholder="شماره همراه را وارد کنید 09127654321"
+                         value={formik.values.phoneNumber}
+                         onChange={formik.handleChange}
+                         name="phoneNumber"/>
+                        <button onClick={() => formik.errors.phoneNumber? null : setViewModal(true)} className="mainRegister__info__input__button">ثبت نام سریع</button>
                     </div>
+                    <p className="inputError__message">{formik.errors.phoneNumber}</p>
+
                 </div>
             </div>
             {viewModal
