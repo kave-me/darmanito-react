bd43b5454bcac29ee23d5aaf040762522339de12
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 20:43:14 +0330

    change RegisterModal Component  styling method

--- src/components/base/modal/contactUs-modal/register-modal.js
+++ src/components/base/modal/contactUs-modal/register-modal.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PharmacyPicture from '../../../../asset/image/pharmacy.svg';
-import './register-modal.scss';
+import styles from './register-modal.module.scss';
 
 const MainDownloadModal = (props) => {
     return(
-        <div className="mainDownloadModal" onClick={props.click}>
-            <div className="mainDownloadModal__card">
-                <img src={PharmacyPicture} alt="pharmacy" className="mainDownloadModal__card__img"/>
-                <h2 className="mainDownloadModal__card__title">پیام شما با موفقیت ارسال شد</h2>
-                <p className="mainDownloadModal__card__desc">برای اعلام توضیحات با شما تماس خواهیم گرفت.</p>
-                <button className="mainDownloadModal__card__button">بستن</button>
+        <div className={styles.mainDownloadModal} onClick={props.click}>
+            <div className={styles.mainDownloadModal__card}>
+                <img src={PharmacyPicture} alt="pharmacy" className={styles.mainDownloadModal__card__img}/>
+                <h2 className={styles.mainDownloadModal__card__title}>پیام شما با موفقیت ارسال شد</h2>
+                <p className={styles.mainDownloadModal__card__desc}>برای اعلام توضیحات با شما تماس خواهیم گرفت.</p>
+                <button className={styles.mainDownloadModal__card__button}>بستن</button>
             </div>
         </div>
     );
