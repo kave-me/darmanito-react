e0a867d0fdd3236c408da9e283b11f5a7dfabb93
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 18:10:34 +0330

    change aboutUsStatics  styling method

--- src/components/aboutUs/statics/aboutUs-statics.js
+++ src/components/aboutUs/statics/aboutUs-statics.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import './aboutUs-statics.scss';
+import styles from './aboutUs-statics.module.scss';
+import cx from 'classnames';
 
 const AboutUsStatics = () => {
     return(
-        <div className="aboutUsStatics section">
-            <div className="aboutUsStatics__card">
-                <p className="aboutUsStatics__card__title">تعداد کاربران درمانیتو</p>
-                <p className="aboutUsStatics__card__number">+120,000</p>
+        <div className={cx(styles.aboutUsStatics, styles.section)}>
+            <div className={styles.aboutUsStatics__card}>
+                <p className={styles.aboutUsStatics__card__title}>تعداد کاربران درمانیتو</p>
+                <p className={styles.aboutUsStatics__card__number}>+120,000</p>
             </div>
-            <div className="aboutUsStatics__card">
-                <p className="aboutUsStatics__card__title">تعداد داروخانه و مراکز</p>
-                <p className="aboutUsStatics__card__number">+120,000</p>
+            <div className={styles.aboutUsStatics__card}>
+                <p className={styles.aboutUsStatics__card__title}>تعداد داروخانه و مراکز</p>
+                <p className={styles.aboutUsStatics__card__number}>+120,000</p>
             </div>
-            <div className="aboutUsStatics__card">
-                <p className="aboutUsStatics__card__title">تعداد فروشگاه ها</p>
-                <p className="aboutUsStatics__card__number">+120,000</p>
+            <div className={styles.aboutUsStatics__card}>
+                <p className={styles.aboutUsStatics__card__title}>تعداد فروشگاه ها</p>
+                <p className={styles.aboutUsStatics__card__number}>+120,000</p>
             </div>
         </div>
         );
