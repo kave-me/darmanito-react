465c96641f7f53c4518315f0f519ca967e62289c
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 17:51:40 +0330

    change sibAppDownloadButton  styling method

--- src/components/base/button/sibApp/sibApp-download-button.js
+++ src/components/base/button/sibApp/sibApp-download-button.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './sibApp-download-button.scss';
 import sibAppLogo from '../../../../asset/image/sib-app-logo.svg';
 import sibAppText from '../../../../asset/image/sib-app-text.svg';
+import styles from './sibApp-download-button.module.scss';
+import cx from 'classnames';
 
 const SibAppDownloadButton = (props) => {
     return(
-        <a href={props.href} className="sibAppButtonContainer" target="_blank" rel="noreferrer">
-            <button className="sibAppButton" style={{backgroundColor: props.backgroundColor}}>
-                    <img className="sibAppButton__text__img" src={sibAppLogo} alt="sibAppLogo"/>
-                <div className="sibAppButton__text">
-                    <p className="sibAppButton__text__text p2">دریافت نسخه ios از</p>
-                    <img className="sibAppButton__logoText" src={sibAppText} alt="sibAppLogoText"/>
+        <a href={props.href} className={styles.sibAppButtonContainer} target="_blank" rel="noreferrer">
+            <button className={styles.sibAppButton} style={{backgroundColor: props.backgroundColor}}>
+                    <img className={styles.sibAppButton__text__img} src={sibAppLogo} alt="sibAppLogo"/>
+                <div className={styles.sibAppButton__text}>
+                    <p className={cx(styles.sibAppButton__text__text, styles.p2)}>دریافت نسخه ios از</p>
+                    <img className={styles.sibAppButton__logoText} src={sibAppText} alt="sibAppLogoText"/>
                 </div>
             </button>
         </a>
