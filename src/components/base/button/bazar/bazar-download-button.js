271ca30117391abc66cfd6b6eff7dae2b8135637
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 17:49:49 +0330

    change bazarDownloadButton  styling method

--- src/components/base/button/bazar/bazar-download-button.js
+++ src/components/base/button/bazar/bazar-download-button.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './bazar-download-button.scss';
 import bazarLogo from '../../../../asset/image/bazzar-logo.svg';
 import bazarText from '../../../../asset/image/bazzar-text.svg';
+import styles from './bazar-download-button.module.scss';
+import cx from 'classnames';
 
 const BazarDownloadButton = (props) => {
     return(
-        <a className="bazarButtonContainer" href={props.href} target="_blank" rel="noreferrer">
-            <button className="bazarButton" style={{backgroundColor: props.backgroundColor}}>
-                <p className="bazarButton__text p2">دریافت از</p>
-                <img src={bazarText} alt="Bazar text" className="bazarButton__logoText"/>
-                <img src={bazarLogo} alt="Bazar logo" className="bazarButton__logo"/>
+        <a className={styles.bazarButtonContainer} href={props.href} target="_blank" rel="noreferrer">
+            <button className={styles.bazarButton} style={{backgroundColor: props.backgroundColor}}>
+                <p className={cx(styles.bazarButton__text, styles.p2)}>دریافت از</p>
+                <img src={bazarText} alt="Bazar text" className={styles.bazarButton__logoText}/>
+                <img src={bazarLogo} alt="Bazar logo" className={styles.bazarButton__logo}/>
             </button>
         </a>
     );
