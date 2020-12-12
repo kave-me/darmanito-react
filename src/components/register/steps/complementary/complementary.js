12168af35cb3468467cb34f501afcac7dc656640
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 20:33:12 +0330

    change complementary form page  styling method

--- src/components/register/steps/complementary/complementary.js
+++ src/components/register/steps/complementary/complementary.js
@@ -2,13 +2,15 @@ import React from 'react';
 import shortid from 'shortid';
 import Card from '../../../aboutUs/header/aboutUs-header-card';
 import ProgressBar from '../../../base/progress-bar/progress-bar';
-import './complementary.scss';
+import styles from './complementary.module.scss';
+import cx from 'classnames';
 
-const invalidInput = (error, _class) => {
-    const result = error
-                   ? `${_class}  inputError`
+const invalidInput = (formik, id, _class) => {
+    
+    const result = formik.errors[id]
+                   ? cx(_class,  styles.inputError)
                    : _class
-    return(result);
+    return(formik.initialValues[id] !== formik.values[id] ? result: _class);
 }
 const isValid = (errors) => {
     return errors.fullAddress ? false :
@@ -17,8 +19,8 @@ const isValid = (errors) => {
 
 const Complementary = (props) => {
     return(
-        <div className="RegisterPharmacyComplementaryFormContainer">
-        <div className="RegisterPharmacyComplementaryFormContainer__cards">
+        <div className={styles.RegisterPharmacyComplementaryFormContainer}>
+        <div className={styles.RegisterPharmacyComplementaryFormContainer__cards}>
         <Card
                 width={100}
                 opacity={1}
@@ -45,29 +47,29 @@ const Complementary = (props) => {
                 id= {shortid.generate()}/>
         </div>
 
-        <form className="RegisterPharmacyComplementaryFormContainer__form">
-            <div className="RegisterPharmacyComplementaryFormContainer__form__head">
-                <h3 className="RegisterPharmacyComplementaryFormContainer__form__title">عضویت به عنوان داروخانه</h3>
-                <div className="RegisterPharmacyComplementaryFormContainer__form__progressBar">
+        <form className={styles.RegisterPharmacyComplementaryFormContainer__form}>
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__head}>
+                <h3 className={styles.RegisterPharmacyComplementaryFormContainer__form__title}>عضویت به عنوان داروخانه</h3>
+                <div className={styles.RegisterPharmacyComplementaryFormContainer__form__progressBar}>
                     <ProgressBar
                     value={76}/>
                 </div>
             </div>
-            <div className="RegisterPharmacyComplementaryFormContainer__form__city">
-                <label className="RegisterPharmacyComplementaryFormContainer__form__city__label"
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__city}>
+                <label className={styles.RegisterPharmacyComplementaryFormContainer__form__city__label}
                 htmlFor="city">شهر</label>
                 <select  onChange={props.formik.handleChange}
-                className="RegisterPharmacyComplementaryFormContainer__form__city__input" name="city"
+                className={styles.RegisterPharmacyComplementaryFormContainer__form__city__input} name="city"
                 value={props.formik.values.city}>
                     <option value="Tehran">تهران</option>
                     <option value="Alborz">البرز</option>
                 </select>
             </div>
-            <div className="RegisterPharmacyComplementaryFormContainer__form__district">
-                <label className="RegisterPharmacyComplementaryFormContainer__form__district__label"
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__district}>
+                <label className={styles.RegisterPharmacyComplementaryFormContainer__form__district__label}
                 htmlFor="district">منطقه</label>
                 <select  onChange={props.formik.handleChange}
-                className="RegisterPharmacyComplementaryFormContainer__form__district__input" name="district"
+                className={styles.RegisterPharmacyComplementaryFormContainer__form__district__input} name="district"
                 value={props.formik.values.district}>
                     <option value="TehranD1" >منطقه یک تهران</option>
                     <option value="TehranD2">منطقه دو تهران</option>
@@ -79,31 +81,31 @@ const Complementary = (props) => {
                     <option value="AlborzD4">منطقه چهار البرز</option>
                 </select>
             </div>
-            <div className="RegisterPharmacyComplementaryFormContainer__form__address">
-                <label className="RegisterPharmacyComplementaryFormContainer__form__address__label"
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__address}>
+                <label className={styles.RegisterPharmacyComplementaryFormContainer__form__address__label}
                 htmlFor="fullAddress">آدرس دقیق</label>
                 <input onChange={props.formik.handleChange}
-                className={invalidInput(props.formik.errors.fullAddress, "RegisterPharmacyComplementaryFormContainer__form__address__input")}
+                className={invalidInput(props.formik, "fullAddress", styles.RegisterPharmacyComplementaryFormContainer__form__address__input)}
                 name="fullAddress"
                 value={props.formik.values.fullAddress}/>
-                <p className="inputError__message">{props.formik.errors.fullAddress}</p>
+                <p className={styles.inputError__message}>{props.formik.errors.fullAddress}</p>
             </div>
-                <p className="RegisterPharmacyComplementaryFormContainer__form__workingHour__label">ساعات کاری</p>
-            <div className="RegisterPharmacyComplementaryFormContainer__form__workingHour"
+                <p className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour__label}>ساعات کاری</p>
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour}
             >
-                <div className="RegisterPharmacyComplementaryFormContainer__form__workingHour__box">
+                <div className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour__box}>
                     <label htmlFor="nonBoarding">روزانه</label>
                     <input type='radio' 
-                    className="RegisterPharmacyComplementaryFormContainer__form__workingHour__input"
+                    className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour__input}
                     checked={props.formik.values.isBoarding === false}
                     name="isBoarding" value={props.formik.values.isBoarding} id="nonBoarding" onChange={e => props.formik.setFieldValue('isBoarding', false)}/>
                 </div>
 
-                <div className="RegisterPharmacyComplementaryFormContainer__form__workingHour__box"
+                <div className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour__box}
                  >
                     <label htmlFor="boarding">شبانه روزی</label>
                     <input type='radio'
-                    className="RegisterPharmacyComplementaryFormContainer__form__workingHour__input"
+                    className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHour__input}
                     checked={props.formik.values.isBoarding === true}
                     name="isBoarding" value={props.formik.values.isBoarding} id="boarding"
                     onChange={e => props.formik.setFieldValue('isBoarding', true)}/>
@@ -112,27 +114,27 @@ const Complementary = (props) => {
 
             {!props.formik.values.isBoarding ?
             <div>
-            <label className="RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__label"
+            <label className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__label}
             htmlFor="workingHourDuration">ساعات کاری</label>
-            <div className="RegisterPharmacyComplementaryFormContainer__form__workingHourDuration">
+            <div className={styles.RegisterPharmacyComplementaryFormContainer__form__workingHourDuration}>
                 <div>
                     <input onChange={props.formik.handleChange}
-                    className={(props.formik.errors.openingHour ,"RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__input")}
+                    className={invalidInput(props.formik, "openingHour" , styles.RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__input)}
                     placeholder="از"
                     name="openingHour"
                     value={props.formik.values.openingHour}/>
-                    <p className="inputError__message">{props.formik.errors.openingHour}</p>
+                    <p className={styles.inputError__message}>{props.formik.errors.openingHour}</p>
                 </div>
                 <div>
                     <input onChange={props.formik.handleChange}
-                    className={(props.formik.errors.closingHour ,"RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__input")}
+                    className={invalidInput(props.formik, "closingHour" , styles.RegisterPharmacyComplementaryFormContainer__form__workingHourDuration__input)}
                     placeholder="تا"
                     name="closingHour"
                     value={props.formik.values.closingHour}/>
-                    <p className="inputError__message">{props.formik.errors.closingHour}</p>
+                    <p className={styles.inputError__message}>{props.formik.errors.closingHour}</p>
                 </div>
             </div></div> :null}
-        <button className="RegisterPharmacyComplementaryFormContainer__form__button"
+        <button className={styles.RegisterPharmacyComplementaryFormContainer__form__button}
         disabled={!isValid(props.formik.errors)} onClick={props.nextStep}>مرحله بعد</button>
         </form>
         </div>
