366ddb8b7b99765b8d68af61f8dd9484e7f09089
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 17:34:34 +0330

    change mainRegister page styling method

--- src/components/main/register/main-register.js
+++ src/components/main/register/main-register.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import mainRegisterPic from '../../../asset/image/mainRegister-person.png';
 import RegisterModal from '../../base/modal/register-modal';
-import './main-register.scss';
+import styles from './main-register.module.scss';
 import {useFormik} from 'formik';
 import * as Yup from 'yup';
 
@@ -16,22 +16,22 @@ const MainRegister = () => {
             .typeError("فقط از اعداد استفاده کنید")})});
     const [viewModal, setViewModal] = useState(false);
     return(
-        <div className="mainRegisterContainer">
-            <div className="mainRegister">
-                <img src={mainRegisterPic} alt="person" className="mainRegister__img"/>
-                <div className="mainRegister__info">
-                    <h2 className="mainRegister__info__title">عضویت داروخانه یا مراکز درمانی</h2>
-                    <p className="mainRegister__info__description">اگر به عنوان داروخانه یا مراکز درمانی قصد دارید در روند درمان و کسب و کار خودتان تغییر ایجاد کنید درمانیتو منتظر شماست.</p>
-                    <div className="mainRegister__info__input">
-                        <input type="text" className="mainRegister__info__input__input"
+        <div className={styles.mainRegisterContainer}>
+            <div className={styles.mainRegister}>
+                <img src={mainRegisterPic} alt="person" className={styles.mainRegister__img}/>
+                <div className={styles.mainRegister__info}>
+                    <h2 className={styles.mainRegister__info__title}>عضویت داروخانه یا مراکز درمانی</h2>
+                    <p className={styles.mainRegister__info__description}>اگر به عنوان داروخانه یا مراکز درمانی قصد دارید در روند درمان و کسب و کار خودتان تغییر ایجاد کنید درمانیتو منتظر شماست.</p>
+                    <div className={styles.mainRegister__info__input}>
+                        <input type="text" className={styles.mainRegister__info__input__input}
                          placeholder="شماره همراه را وارد کنید 09127654321"
                          value={formik.values.phoneNumber}
                          onChange={formik.handleChange}
                          onBlur={formik.handleBlur}
                          name="phoneNumber"/>
-                        <button onClick={() => !formik.errors.phoneNumber && formik.values.phoneNumber !== ""?setViewModal(true): null} className="mainRegister__info__input__button">ثبت نام سریع</button>
+                        <button onClick={() => !formik.errors.phoneNumber && formik.values.phoneNumber !== ""?setViewModal(true): null} className={styles.mainRegister__info__input__button}>ثبت نام سریع</button>
                     </div>
-                    <p className="inputError__message">{formik.errors.phoneNumber}</p>
+                    <p className={styles.inputError__message}>{formik.errors.phoneNumber}</p>
 
                 </div>
             </div>
