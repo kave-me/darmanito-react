b706ee3ca89c9f0693177647d528ee85c4674e09
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 18:16:17 +0330

    change ContactUs page  styling method

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -5,9 +5,9 @@ import Footer from '../components/base/footer';
 import SimpleMap from '../components/contactUs/map';
 import {useFormik} from 'formik';
 import * as Yup from 'yup';
-import './contactUs.scss';
 import RegisterModal from '../components/base/modal/contactUs-modal';
-
+import styles from './contactUs.module.scss';
+import cx from 'classnames';
 
 const ContactUs = () => {
     const formik = useFormik(
@@ -32,49 +32,49 @@ const ContactUs = () => {
     );
     const [viewModal, setViewModal] = useState(false);
     return (
-        <div className="container">
+        <div className={styles.container}>
                         {viewModal
              ? <RegisterModal click={() => setViewModal(!viewModal)}/>
              : null}
             <TextBanner text="تماس با ما"/>
-            <div className="contactUsFormAndMap section">
-                <div className="messageForm">
-                    <form className="messageForm__form">
-                        <label className="messageForm__form__label" htmlFor="fullName">نام و نام خانوادگی</label>
-                        <input className="messageForm__form__input" id="fullName" name="fullName" type="text" placeholder="نام و نام خانوادگی"
+            <div className={cx(styles.contactUsFormAndMap, styles.section)}>
+                <div className={styles.messageForm}>
+                    <form className={styles.messageForm__form}>
+                        <label className={styles.messageForm__form__label} htmlFor="fullName">نام و نام خانوادگی</label>
+                        <input className={styles.messageForm__form__input} id="fullName" name="fullName" type="text" placeholder="نام و نام خانوادگی"
                         onChange={formik.handleChange}/>
-                        <p className="inputError__message">{formik.errors.fullName}</p>
+                        <p className={styles.inputError__message}>{formik.errors.fullName}</p>
 
-                        <label className="messageForm__form__label" htmlFor="emailAndPhone">ایمیل یا شماره همراه</label>
-                        <input className="messageForm__form__input" id="emailAndPhone" type="text" placeholder="ایمیل یا شماره همراه"
+                        <label className={styles.messageForm__form__label} htmlFor="emailAndPhone">ایمیل یا شماره همراه</label>
+                        <input className={styles.messageForm__form__input} id="emailAndPhone" type="text" placeholder="ایمیل یا شماره همراه"
                         onChange={formik.handleChange} name="emailAndPhone"/>
-                        <p className="inputError__message">{formik.errors.emailAndPhone}</p>
+                        <p className={styles.inputError__message}>{formik.errors.emailAndPhone}</p>
 
 
-                        <label className="messageForm__form__label" htmlFor="message">پیام شما</label>
-                        <textarea className="messageForm__form__textarea" name="message" id="message" cols="30" rows="9" placeholder="متن پیام"
+                        <label className={styles.messageForm__form__label} htmlFor="message">پیام شما</label>
+                        <textarea className={styles.messageForm__form__textarea} name="message" id="message" cols="30" rows="9" placeholder="متن پیام"
                         onChange={formik.handleChange}></textarea>
-                        <p className="inputError__message">{formik.errors.message}</p>
+                        <p className={styles.inputError__message}>{formik.errors.message}</p>
 
-                        <button type="button" className="messageForm__form__button" onClick={() => formik.isValid &&formik.values.emailAndPhone!== formik.initialValues.emailAndPhone ?setViewModal(true):null}>ارسال پیام</button>
+                        <button type="button" className={styles.messageForm__form__button} onClick={() => formik.isValid &&formik.values.emailAndPhone!== formik.initialValues.emailAndPhone ?setViewModal(true):null}>ارسال پیام</button>
                     </form>
                 </div>
-                <div className="contactUS__map"> 
+                <div className={styles.contactUS__map}> 
                 <SimpleMap>
-                    <div className="contactUs__map__cover">
-                        <div className="contactUs__map__cover__emailAndPhoneNumberContainer">
-                            <div className="contactUs__map__cover__email">
-                                <p className="contactUs__map__cover__email__label">ایمیل</p>
-                                <p className="contactUs__map__cover__email__value">info@nahiratech.com</p>
+                    <div className={styles.contactUs__map__cover}>
+                        <div className={styles.contactUs__map__cover__emailAndPhoneNumberContainer}>
+                            <div className={styles.contactUs__map__cover__email}>
+                                <p className={styles.contactUs__map__cover__email__label}>ایمیل</p>
+                                <p className={styles.contactUs__map__cover__email__value}>info@nahiratech.com</p>
                             </div>
-                            <div className="contactUs__map__cover__phoneNumber">
-                                <p className="contactUs__map__cover__phoneNumber__label">شماره ثابت</p>
-                                <p className="contactUs__map__cover__phoneNumber__value">021-44219671</p>
+                            <div className={styles.contactUs__map__cover__phoneNumber}>
+                                <p className={styles.contactUs__map__cover__phoneNumber__label}>شماره ثابت</p>
+                                <p className={styles.contactUs__map__cover__phoneNumber__value}>021-44219671</p>
                             </div>
                         </div>
-                        <div className="contactUs__map__cover__address">
-                            <p className="contactUs__map__cover__address__label">آدرس</p>
-                            <p className="contactUs__map__cover__address__value">تهران، طرشت، خیابان شهید چوب تراش، خیابان شهید حسین مردی، بن بست پنجم، پلاک 15، واحد 1</p>
+                        <div className={styles.contactUs__map__cover__address}>
+                            <p className={styles.contactUs__map__cover__address__label}>آدرس</p>
+                            <p className={styles.contactUs__map__cover__address__value}>تهران، طرشت، خیابان شهید چوب تراش، خیابان شهید حسین مردی، بن بست پنجم، پلاک 15، واحد 1</p>
                         </div>
 
                     </div>
