0ae1c6ac750f45eef3a3aa8c837ae2c5b231f386
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 17:55:10 +0330

    change AndroidDownloadButton  styling method

--- src/components/base/button/android/android-download-button.js
+++ src/components/base/button/android/android-download-button.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './android-download-button.scss';
 import androidLogo from '../../../../asset/image/android.svg';
+import styles from './android-download-button.module.scss';
 
 const AndroidDownloadButton = (props) => {
     return(
-        <a className="androidButtonContainer" href={props.href} target="_blank" rel="noreferrer">
-            <button className="androidButton" style={{backgroundColor: props.backgroundColor}}>
-                <img src={androidLogo} alt="Android logo" className="androidButton__logo"/>
-                <p className="androidButton__text">دانلود مستقیم</p>
+        <a className={styles.androidButtonContainer} href={props.href} target="_blank" rel="noreferrer">
+            <button className={styles.androidButton} style={{backgroundColor: props.backgroundColor}}>
+                <img src={androidLogo} alt="Android logo" className={styles.androidButton__logo}/>
+                <p className={styles.androidButton__text}>دانلود مستقیم</p>
             </button>
         </a>
     );
