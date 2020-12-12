4f213ed8575c5429328afc9a86f36d9432abf105
Kave Mohammadi <kave.mdi@gmail.com>
2020-12-12 17:37:58 +0330

    change footer  styling method

--- src/components/base/support/support.js
+++ src/components/base/support/support.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import supportLogo from '../../../asset/image/support-area-bt.png';
-import './support.scss';
+import styles from './support.module.scss';
+import cx from 'classnames';
 
 const Support = () => {
     return(
-        <div className="section support">
-            <div className="support__text">
-                <p className="support__text__title">پشتیبانی درمانیتو</p>
-                <p className="support__text__body p2">پاسخگویی در روزهای کاری از ساعت 9 صبح تا 9 شب</p>
+        <div className={cx(styles.section, styles.support)}>
+            <div className={styles.support__text}>
+                <p className={styles.support__text__title}>پشتیبانی درمانیتو</p>
+                <p className={cx(styles.support__text__body, styles.p2)}>پاسخگویی در روزهای کاری از ساعت 9 صبح تا 9 شب</p>
             </div>
-            <button className="support__button">
-                <img src={supportLogo} alt="support icon" className="support__button__img"/>
-                <p className="support__button__text p2"><a className="support__button__text__a" href="tel:009844219671">پشتیبانی:   02144219671</a></p>
+            <button className={styles.support__button}>
+                <img src={supportLogo} alt="support icon" className={styles.support__button__img}/>
+                <p className={cx(styles.support__button__text, styles.p2)}><a className={styles.support__button__text__a} href="tel:009844219671">پشتیبانی:   02144219671</a></p>
             </button>
         </div>
     );
